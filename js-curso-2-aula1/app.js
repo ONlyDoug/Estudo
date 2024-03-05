14f6c71758ffedab59b35a65c3b1dcfe796daf36
Doug <doug@inovabytes.com>
2024-03-05 17:15:41 -0300

    Update app.js

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = "jogo do numero secreto";
 
 let paragrafo = document.querySelector("p");
 paragrafo.innerHTML = "Escolha um numero entre 1 e 10";
+
+function verificarChute() {
+  console.log("O botão foi clicado!");
+}
